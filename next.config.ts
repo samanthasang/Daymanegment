abe8d35accd2c961346f2edd3dfe38d4d6f23d5e
samanthasang <samanthasang70@gmail.com>
2026-06-16 18:36:48 +0330

    fix files

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
+// next.config.ts
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // If you use next/image, you also need to disable default image optimization:
+  images: { unoptimized: true },
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/sw.js",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
